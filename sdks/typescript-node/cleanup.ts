16f9bad741511e177e90314a6c6e5ea11f308be7
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2023-11-07 13:22:28 +0100

    chore: automate a search & replace in one of the generated node files

--- sdks/typescript-node/cleanup.ts
+++ sdks/typescript-node/cleanup.ts
@@ -1,5 +1,12 @@
 import { replaceStringInFilesSync } from 'tiny-replace-files';
 
+/**
+ * One of our responses contain a property with the name 'symbol'. Our generator creates a very usable output
+ * but it's not smart enough to recognize that 'symbol' is a reserved keyword and generates an enum based on the property.
+ *
+ * Therefore, we automatically replace 'symbol' for 'currencySymbol' and call it a day.
+ */
+
 const options = {
   files: 'src/generated/models/GetAssetResponse.ts',
   from: 'symbol',
