d06709958ba63d3697cfbedbc09ec4badf46786c
Maciej Lewinski <maciej@lewinski.dev>
2023-11-09 14:40:13 +0100

    Move csharp library generation to generichost to fix nullability issues

---
+++ sdks/csharp/src/Beam/Api/IApi.cs
@@ -0,0 +1,15 @@
+using System.Net.Http;
+
+namespace Beam.Api
+{
+    /// <summary>
+    /// Any Api client
+    /// </summary>
+    public interface IApi
+    {
+        /// <summary>
+        /// The HttpClient
+        /// </summary>
+        HttpClient HttpClient { get; }
+    }
+}
